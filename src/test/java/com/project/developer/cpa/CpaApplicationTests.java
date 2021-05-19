7b201d5ca7fae6596501dd47a953ccb667bb5c14
Deepalakshmi, Balachandran <saideepa06@gmail.com>
2021-05-19 12:37:51 +0530

    Spring boot sample app files

---
+++ src/test/java/com/project/developer/cpa/CpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.developer.cpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
