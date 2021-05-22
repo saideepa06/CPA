d39560a1e9e789e8e5c70652767d1839742a4689
Deepalakshmi, Balachandran <saideepa06@gmail.com>
2021-05-22 19:45:02 +0530

    Unused imports removed

--- src/main/java/com/project/developer/cpa/config/Initializer.java
+++ src/main/java/com/project/developer/cpa/config/Initializer.java
@@ -2,7 +2,6 @@ package com.project.developer.cpa.config;
 
 import com.project.developer.cpa.model.CreditCard;
 import com.project.developer.cpa.repository.CardRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -16,7 +15,7 @@ public class Initializer implements CommandLineRunner {
     }
 
     @Override
-    public void run(String... args) throws Exception {
+    public void run(String... args) {
 
         repository.save(new CreditCard(4003600000000014L, "Deepalakshmi", 200000L, 0L));
 
